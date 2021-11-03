c9e2c692245216aff24a29b826d61004f1ff181b
RykovV <kcziuy@gmail.com>
2021-11-03 20:06:42 +0300

    [fix] virtualization missed

--- main.tf
+++ main.tf
@@ -19,10 +19,10 @@ data "aws_ami" "ubuntu" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
-  # filter {
-  #   name   = "virtualization-type"
-  #   values = ["hvm"]
-  # }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
   owners = ["099720109477"] # Canonical
 }
