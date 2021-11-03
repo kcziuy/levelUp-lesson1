6dba8046cdebdfa35c027a0530d9188c2ad1a0fe
RykovV <kcziuy@gmail.com>
2021-11-03 18:00:17 +0300

    [add] vm created, no ssh, no ngnix

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
-provider "aws" {
-  region = "eu-central-1"
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+resource "aws_key_pair" "kp" {
+  key_name = "key1"
+  public_key = tls_private_key.pk.public_key_openssh
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.pk.private_key_pem}' > key.pem"
+  }
+}
+
+resource "aws_instance" "debian-vm-1" {
+  ami = "ami-0623a3b0ec93b4fb7"
+  key_name = aws_key_pair.kp.key_name
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyFirstDebian"
+  }
 }
